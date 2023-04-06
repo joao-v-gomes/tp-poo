dea873e16b43721691e6cb6103ab53e12b241a5c
Joao Gomes <joao.victor214@gmail.com>
2023-04-06 18:08:41 -0300

    Fazendo a classe Aeronave

---
+++ classAeronave.php
@@ -0,0 +1,15 @@
+<?php
+class Aeronave
+{
+	private string $fabricante;
+	private string $modelo;
+	private string $capacidadePassageiros;
+	private float $capacidadeCargaKg;
+	private string $registro;
+	private bool $disponivel;
+
+
+	public function __construct()
+	{
+	}
+}
