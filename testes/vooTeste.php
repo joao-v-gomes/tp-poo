1d442c95dc8201307281edbbb171826e5cfbb931
Joao Gomes <joao.victor214@gmail.com>
2023-04-11 21:47:32 -0300

    Organizando os arquivos no repositorio

---
+++ testes/vooTeste.php
@@ -0,0 +1,10 @@
+<?php
+include_once("../classVoo.php");
+include_once("../classAeronave.php");
+
+$hp = new DateTime('now');
+$hc = new DateTime('now');
+$topgun = new Aeronave("russian", "mk-dir", 500, 10000, "a123", true);
+$galera = array("Milton", "Irineu", "Wesley");
+$voandoHigh = new Voo("Confins", "Congonhas", $hp, $hc, 24, "Latam", $topgun, $galera, 4500);
+print_r($voandoHigh);
