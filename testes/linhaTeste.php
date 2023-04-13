8f5be2f2c290d4f8a0ef27652652f27218764904
Joao Gomes <joao.victor214@gmail.com>
2023-04-13 18:06:11 -0300

    Finalizando teste da linha

--- testes/linhaTeste.php
+++ testes/linhaTeste.php
@@ -6,9 +6,15 @@ $conjunto = array(15, 14, 12);
 $pp = new DateTime('now');
 $pc = new DateTime('now');
 
-$linhaBrazuca = new Linha($conjunto, $pp, $pc, 300);
+$hp = new DateTime('now');
+$hc = new DateTime('now');
+
+$topgun = new Aeronave("russian", "mk-dir", 500, 10000, "a123", true);
+
+$galera = array("Milton", "Irineu", "Wesley");
+
+$voandoHigh = new Voo("Confins", "Congonhas", $hp, $hc, 24, "Latam", $topgun, $galera, 4500);
+
+$linhaBrazuca = new Linha($conjunto, $pp, $pc, 300, $voandoHigh);
 
 print_r($linhaBrazuca);
-?>
-
-<!-- array $frequencia, DateTime $previsaoPartida, DateTime $previsaoChegada, float $previsaoDuracao, Voo $voo -->
