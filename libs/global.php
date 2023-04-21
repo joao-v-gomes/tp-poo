78abf4b92cde81ef8241ebedad525b227106ec29
Joao Gomes <joao.victor214@gmail.com>
2023-04-21 11:20:24 -0300

    Fazendo o global funcionar melhor

--- libs/global.php
+++ libs/global.php
@@ -4,21 +4,41 @@ function autoloader($pClassName)
     echo __NAMESPACE__;
     $path = '';
 
+    $teste_path = __DIR__;
+
+    // print_r($teste_path);
+
+    if (str_contains($teste_path, "joao")) {
+        // print_r("Achou joao");
+        $path = '/home/joao/ufmg/poo/tp-poo/';
+    } else {
+        // print_r("Não achou joao");
+        $path = '/home/runner/tp-poo/';
+    }
+
     if ($pClassName == 'persist') {
+
+
+        $path = $path . 'libs/' . $pClassName . '.php';
+
         // CAMINHO REPLIT
         // $path = '/home/runner/tp-poo/libs/' . $pClassName . '.php';
 
         // CAMINHO JOAO
-        $path = '/home/joao/ufmg/poo/tp-poo/libs/' . $pClassName . '.php';
+        // $path = '/home/joao/ufmg/poo/tp-poo/libs/' . $pClassName . '.php';
     } else {
+
+
+        $path = $path . 'classes/' . 'class' . $pClassName . '.php';
+
         // CAMINHO REPLIT
         // $path = '/home/runner/tp-poo/' . 'class' . $pClassName . '.php';
 
         // CAMINHO JOAO
-        $path = '/home/joao/ufmg/poo/tp-poo/' . 'class' . $pClassName . '.php';
+        // $path = '/home/joao/ufmg/poo/tp-poo/' . 'class' . $pClassName . '.php';
     }
 
-    print_r($path);
+    print_r('Caminho: ' . $path);
 
     if (is_file($path)) {
         include_once $path;
