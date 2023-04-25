3ae4d8b102c5ecdf740da2b8562554453582ac0d
Joao Gomes <joao.victor214@gmail.com>
2023-04-25 17:47:31 -0300

    Criacao de novos txts

--- classes/classPassagem.php
+++ classes/classPassagem.php
@@ -11,6 +11,8 @@ class Passagem extends persist
   private Passageiro $passageiro;
   private Cliente $cliente;
 
+  static $local_filename = "passagens.txt";
+
   public function __construct(string $siglaAeroportoOrigem, string $siglaAeroportoDestino, float $preco, string $assento, int $franquiasBagagem, Passageiro $passageiro, Cliente $cliente)
   {
     $this->setSiglaAeroportoOrigem($siglaAeroportoOrigem);
